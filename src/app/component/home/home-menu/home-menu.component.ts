560a7deb629f4e53a6150f81142c70a149bc5ff3
Joza <joseicew@gmail.com>
2018-11-30 11:50:18 +0100

    Home Menu History

--- src/app/component/home/home-menu/home-menu.component.ts
+++ src/app/component/home/home-menu/home-menu.component.ts
@@ -7,7 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeMenuComponent implements OnInit {
 
-  constructor() { }
+  private buy_array:any=[];
+  
+  constructor() {
+    if (localStorage.getItem('LocalUser')) {
+      let user = JSON.parse(localStorage.getItem('LocalUser'));
+      let buy_board: any = [];
+      buy_board = user.buy_board;
+      
+      for (let i = 0; i < Object.keys(buy_board).length; i++) 
+      {
+        this.buy_array[i] = buy_board[Object.keys(buy_board)[i]];
+        console.log(this.buy_array[i]);
+
+      }
+    }
+  }
 
   ngOnInit() {
   }
